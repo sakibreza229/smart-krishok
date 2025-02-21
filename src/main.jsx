f79509ee5ee98afb8f156db672dca03854b718db
Sakib Reza <sakibreza229@gmail.com>
2025-02-21 15:25:30 +0600

    initial setup

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,8 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import './main.css'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <>
+    <p className="font-bold text-xl">Hello World</p>
+  </>,
 )
